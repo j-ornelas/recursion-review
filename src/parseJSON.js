dcca565a85a76db8d6e684c0631acaabdb7d70b3
j-ornelas <johnnyoh1988@gmail.com>
2018-01-15 16:01:18 -0800

    we can handle strings in arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,23 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var parseArray = function(stringArr) {
+    var outputArray = [];
+    var wordPlaceholder = '';
+    var outerQuote = true;
+    for (var i = 1; i < stringArr.length - 1; i++) {
+      if ((stringArr[i] === ',' && outerQuote === true ) || i === stringArr.length - 2 ) {
+        outputArray.push(wordPlaceholder);
+        wordPlaceholder = '';
+        i += 1;
+        changeValue(outerQuote);
+      } else if (stringArr[i] === '"') {
+        changeValue(outerQuote);
+      } else {
+        wordPlaceholder += stringArr[i];
+      }
+    }
+
+    return outputArray;
+  };
 };
